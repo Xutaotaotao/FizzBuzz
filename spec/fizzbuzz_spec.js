8fbf12a853f864ee90a5efb1626e1e784a348f8e
Taotao Xu <xutaotao1108@outlook.com>
2017-11-20 21:05:01 +0800

    Adjust code style

--- spec/fizzbuzz_spec.js
+++ spec/fizzbuzz_spec.js
@@ -1,23 +1,23 @@
 "use strict";
 let fizzBuzz = require("../lib/fizzbuzz.js");
 
-describe("FizzBuzz",function () {
-    it("returns the number given number not divisible by neither 3 or 5",function () {
+describe("FizzBuzz", function () {
+    it("returns the number given number not divisible by neither 3 or 5", function () {
         let number = 1;
         let result = fizzBuzz(number);
         expect(result).toEqual("1");
     });
-    it("returns the Fizz given number by 3",function () {
+    it("returns the Fizz given number by 3", function () {
         let number = 3;
         let result = fizzBuzz(number);
         expect(result).toEqual("Fizz");
     });
-    it("returns the Fizz given number by 5",function () {
+    it("returns the Fizz given number by 5", function () {
         let number = 5;
         let result = fizzBuzz(number);
         expect(result).toEqual("Buzz");
     });
-    it("returns the Fizz given number by both 3 and 5",function () {
+    it("returns the Fizz given number by both 3 and 5", function () {
         let number = 15;
         let result = fizzBuzz(number);
         expect(result).toEqual("FizzBuzz");
